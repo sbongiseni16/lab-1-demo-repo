3886c3a98456ea1bab7576d540c2f5cf5a50c68b
Stephen Levitt <stephen.levitt@wits.ac.za>
2024-07-30 18:27:43 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -6,27 +6,28 @@ using namespace std;
 
 int main()
 {
-	auto myScreen = Screen{6,6};
+	auto myScreen = Screen{6, 6};
 	myScreen.forward();
 	myScreen.set('*');
 	myScreen.down();
 	myScreen.set('*');
-	myScreen.move(3,3);
+	myScreen.move(3, 3);
 	myScreen.set("---");
 
 	myScreen.display();
 	cout << endl;
 
-	myScreen.reSize(16,16);
+	myScreen.reSize(16, 16);
 	myScreen.display();
 	myScreen.clear(' ');
 
-	myScreen.move(7,7);
+	myScreen.move(7, 7);
 	myScreen.set("BIG");
-	myScreen.move(8,5);
+	myScreen.move(8, 5);
 	myScreen.set("SCREEN");
 	myScreen.display();
 
 	return 0;
 }
 
+// Exercise 4.1
